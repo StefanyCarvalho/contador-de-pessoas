706f1bb66710cab42d837700394f27b9a70cfae2
Stefany Carvalho <jmenezes241@gmail.com>
2019-08-21 01:55:00 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Contsdor de Pessoas",
+    home: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+      Text("Pessoas: 0"),
+    ],) ,
+  ),);
+}
