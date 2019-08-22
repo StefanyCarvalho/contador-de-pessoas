1add5ee05188df58fb66858ce5bbbbe457c2d36b
Jonathan Vegas Peixoto <jopxoto12@gmail.com>
2019-08-22 02:02:31 -0300

    second version

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "Contsdor de Pessoas",
-    home: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-      Text("Pessoas: 0"),
-    ],) ,
-  ),);
+  runApp(
+    MaterialApp(
+      title: "Contsdor de Pessoas",
+      home: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Image.asset(
+            "images/bg.jpg",
+            fit: BoxFit.cover,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Pessoas: 0",
+                style: TextStyle(
+                  fontSize: 32.0,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ),
+  );
 }
